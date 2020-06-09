0eaa3e9d1298360671d1a26556615f988be9d7cd
github username <r.miriuk@tutanota.com>
2020-06-09 22:10:08 +0100

    fixed typo

--- voice.py
+++ voice.py
@@ -3,7 +3,7 @@ import speech_recognition
 
 recognizer = speech_recognition.Recognizer()
 with speech_recognition.Microphone() as source:
-    print("Hello, what's your name?")
+    print("say something")
     audio = recognizer.listen(source)
 
 words = recognizer.recognize_google(audio)
@@ -11,7 +11,5 @@ words = recognizer.recognize_google(audio)
 matches = re.search("my name is (.*)",words)
 if matches:
     print(f"hey,{matches[1]}.")
-elif re.search('hello',words):
-    print("what's your name")
 else:
-    print('bye')
+    print("hey you.")
